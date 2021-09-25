ce7422198dccd5901f4d577266ca557efb569678
Joseph Siddall <j.b.siddall@gmail.com>
2021-09-25 23:01:00 +0100

    progress

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Of random webpages rebuilt for practice and training
-
-View Portfolio at: https://portfolio-neon-nine.vercel.app/
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,130 @@
 <script setup>
-
-const pages = [
-  {
-    name: "Apple Homepage",
-    github: "https://github.com/jbsiddall/portfolio-apple-homepage",
-    screenshot: "/apple_homepage.png",
-  }
-]
 </script>
 
 <template>
-  <h1>Portfolio</h1>
-  <div class="grid">
-    <div v-for="page in pages" class="card__container" :key="page.name">
-      <img class="card__image" :src="page.screenshot" />
-      <div class="card__info">
-        <p>{{page.name}}</p>
-        <a :href="page.github">github</a>
+  <header>
+  </header>
+
+  <main>
+    <div class="bg-light">
+      <div class="section section--first">
+        <h1 class="text text--h1 text--dark">13 Pro</h1>
+        <h2 class="text text--h2 text--dark">IPhone 13 Pro</h2>
+        <a class="link link--dark">Learn more ></a>
+        <a class="link link--dark">Buy ></a>
       </div>
     </div>
-  </div>
+
+    <div class="bg-dark">
+      <div class="section section--second">
+        <h1 class="text text--h1 text--light">13</h1>
+        <h2 class="text text--h2 text--light">IPhone 13</h2>
+        <a class="link link--light">Learn more ></a>
+        <a class="link link--light">Buy ></a>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+  </footer>
 </template>
 
-<style scoped>
-.grid {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+<style lang="scss">
+@use "sass:map";
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+$breakpoints: (
+  mobile: 734px,
+  tablet: 1068px,
+);
+
+$text_colors: (
+  light: #FFF,
+  dark: #231f20,
+);
+
+$link_colors: (
+  light: #fec2eb,
+  dark: #06c,
+);
+
+$background_colors: (
+  light: #f5f5f7,
+  dark: #35273c,
+);
+
+body {
+  margin: 0px;
+}
+
+* {
+  font-family: Roboto;
+}
+
+.text {
+  margin: 0px;
+  padding: 0px;
+}
+
+.text--h1 {
+  font-weight: 500;
+  font-size: 230px;
+}
+
+.text--h2 {
+  font-weight: 400;
+  font-size: 35px;
+}
+
+.text--dark {
+  color: map.get($text_colors, dark);
+}
+
+.text--light {
+  color: map.get($text_colors, light);
+}
+
+.link {
+  font-weight: 300;
+  font-size: 18px;
+
+  &--dark {
+    color: map.get($link_colors, dark);
+  }
+
+  &--light {
+    color: map.get($link_colors, light);
+  }
+}
+
+.section {
+  margin: 175px auto 0px auto;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
   width: 100%;
-  max-width: 1100px;
-  margin: 100px auto;
+
+  @media (max-width: map.get($breakpoints, tablet)) {
+    max-width: 576px;
+  }
+
+  @media (max-width: map.get($breakpoints, mobile)) {
+    max-width: 283px;
+  }
+
+  &--first {
+  }
+
+  &--second {
+  }
 }
 
-.card__container {
-  width: 200px;
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+.bg-light {
+    background-color: map.get($background_colors, light);
 }
 
-.card__image {
-  width: 200px;
-  height: 200px;
+.bg-dark {
+    background-color: map.get($background_colors, dark);
 }
 
-.card__info {
-  padding: 16px;
-}
 </style>
